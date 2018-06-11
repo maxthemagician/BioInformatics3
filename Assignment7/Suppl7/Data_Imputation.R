27ca8b81c741decaa3eb385fc5bb495f5bed849d
Max Jakob <MaxJakob@icloud.com>
2018-06-11 16:31:21 +0200

    added comments

--- Assignment7/Suppl7/Data_Imputation.R
+++ Assignment7/Suppl7/Data_Imputation.R
@@ -8,10 +8,9 @@ if(!require(ggplot2)){
 dat = fread("ms_toy.txt") #read data
 dat = as.matrix(dat)      #transform fo better plotting
 
-org_dat = dat[,1]
+org_dat = dat[,1]         # select a column
 
-c1 = org_dat
-  #plot of inital dist of col 1
+c1 = org_dat              # copy for plot
 
 s = sd(org_dat, na.rm = T)    # compute standart deviation
 s = 1/4 *s                    # take fraction
@@ -19,17 +18,17 @@ s = 1/4 *s                    # take fraction
 q = quantile(org_dat, na.rm = T)  # compute quantiles
 nm = as.numeric(q[2])             # set new mean to 25% quantile 
 
-filles = list()
-for(i in 1:length(org_dat)){
+filles = list()               # list with new entries
+for(i in 1:length(org_dat)){    # replace missing entries with new entries from new distribution
   if(is.na(org_dat[i])){
     org_dat[i] = rnorm(1, nm, s)
     filles[length(filles)+1]= org_dat[i]
   }
 }
 
-hist(org_dat, col = "Green", xlab = "Expression Value")
-hist(c1, col = "Blue", add = T)
-hist(as.numeric(filles), col = "Red", add=T)
+hist(org_dat, col = "Green", xlab = "Expression Value") # plot complete new distipution with imputed values in green
+hist(c1, col = "Blue", add = T)                         # add inital distribution in blue
+hist(as.numeric(filles), col = "Red", add=T)            # add dist of imputed values
 
 
 
